2bb464791f3e24c8d6b12adf9e5eef4d8ef0f7ff
Justin Ellis <jdellis490@gmail.com>
2022-05-21 21:44:01 -0700

    corrected get and post routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,28 @@
+const { response } = require('express');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 // Routes
 
-// Get route for all the notes
-// GET route to read api notes
+// Get routes for index.html and note.html files
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// GET route to read db.json file
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) throw err;
@@ -21,21 +32,33 @@ app.get('/api/notes', (req, res) => {
 });
 // POST route to post a new note
 app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        if (err) throw err;
-        const parseData = JSON.parse(data);
-        const newNote = req.body;
-        parseData.push(newNote);
-        fs.writeFile('./db/db.json', JSON.stringify(parseData), (err) => {
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uniqid(),
+        };
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
             if (err) throw err;
-            res.json(parseData);
-            console.log('The note has been saved!');
+            const parseData = JSON.parse(data);
+            parseData.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(parseData), (err) => {
+                if (err) throw err;
+                res.json(parseData);
+                console.log('The note has been saved!');
+            });
+            //res.json(parseData);
         });
-        //res.json(parseData);
-    });
+    }  
 });
 
+
+
 // DELETE ROUTE put some this logic in separate function
+app.delete(`/api/notes/${uniqid}`, (req, res) => {
+   
+})
 //getData - might need to be a promise - fs promises
 
 
